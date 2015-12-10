0b397a944bfad8e0382dabbd45b2e64a7a80070f
Icelle <icelleku@gmail.com>
2015-12-10 14:15:23 -0500

    fix(skeptic): make skeptic an optional dependency
    feature `skeptic` will now run tests on README.md

--- build_readme_test.rs
+++ build_readme_test.rs
@@ -1,5 +1,11 @@
+#[cfg(feature = "skeptic")]
 extern crate skeptic;
 
+#[cfg(feature = "skeptic")]
 fn main() {
     skeptic::generate_doc_tests(&["README.md"]);
 }
+
+// do nothing unless skeptic feature is present
+#[cfg(not(feature = "skeptic"))]
+fn main() {}
--- tests/skeptic.rs
+++ tests/skeptic.rs
@@ -1,2 +1,2 @@
-#[cfg(feature = "test-quandl-api")]
+#[cfg(feature = "skeptic")]
 include!(concat!(env!("OUT_DIR"), "/skeptic-tests.rs"));
